67cacab0558e1ddab84bd70d1a45558f2b1fcb23
Jorge Sabella <jorge_andres1996@hotmail.com>
2017-07-21 20:54:09 -0500

    Added all randomized colors

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,10 @@
 //array of colors
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
-];
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square"); //selcting the squares divs and saving them in all the variable squares
 var pickedColor = pickedColor(); //selecting the color that we want to find from the colors array
 var colorDisplay = document.querySelector("#colorDisplay"); //selecting the h1 span for changing its content to the color that we want to find
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor; //changing the span content
 //loop to add content and funcionality of the squares divs
@@ -22,6 +16,7 @@ for(var i = 0; i < squares.length; i++){
 		if(clickedColor === pickedColor){ //compare color to pickedColor
 			messageDisplay.textContent = "Correct!";
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		}else{
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
@@ -40,4 +35,28 @@ function changeColors(color){
 function pickedColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make an array
+	var arr = [];
+	//repeat num times
+	for(var i = 0; i < num; i++){
+		//get random color and push into arr
+		arr.push(generateRandomColorString());
+	}
+	//return that array
+	return arr;
+}
+
+function generateRandomNumber(num){
+	return Math.floor(Math.random() * num);
+}
+
+function generateRandomColorString(){
+	var string = "rgb(";
+	string += generateRandomNumber(256) + ", ";
+	string += generateRandomNumber(256) + ", ";
+	string += generateRandomNumber(256) + ")"; 
+	return string;
 }
